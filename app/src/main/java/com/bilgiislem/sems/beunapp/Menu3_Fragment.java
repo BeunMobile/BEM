3a2bcf85b6bda0ca8b7ba7c32504e64c3acde610
semsturgut <mtdetroit@gmail.com>
2015-03-19 15:48:12 +0200

    BeunTema v0.1

--- app/src/main/java/com/bilgiislem/sems/beunapp/Menu3_Fragment.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/Menu3_Fragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * Created by sems on 17.03.2015.
