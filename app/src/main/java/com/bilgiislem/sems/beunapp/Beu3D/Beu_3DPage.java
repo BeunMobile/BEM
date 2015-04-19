b7fc0f9ef6ffd12f2314a22c3b9a0d9728b7e8ea
semsturgut <mtdetroit@gmail.com>
2015-04-19 13:14:46 +0300

    -Map uygulamasının %80 i tamamlandı .+Güzel görünüyor... -Duyurular %10 -Etkinlikler %10 -PhotosynthBeu %50 -Ogrenci%10 -Beu3D %95 (Tasarımı değişecek)

--- app/src/main/java/com/bilgiislem/sems/beunapp/Beu3D/Beu_3DPage.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/Beu3D/Beu_3DPage.java
@@ -1,43 +1,35 @@
 package com.bilgiislem.sems.beunapp.Beu3D;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.webkit.WebView;
-import android.widget.Toast;
 
 import com.bilgiislem.sems.beunapp.R;
 
-
-public class Beu_3DPage extends ActionBarActivity {
-
+public class Beu_3DPage extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final String AnaUrl = "https://photosynth.net/preview/embed/283b433e-508f-41bd-b385-1a011d358a6c?delayload=false&autoplay=true&fromsite=true";
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ana_sayfa__web_page);
-        WebView webview = (WebView) findViewById(R.id.ana_sayfa_wp);
+        setContentView(R.layout.beu_3dpage_layout);
+        WebView webview = (WebView) findViewById(R.id.beu_3d_page);
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl(AnaUrl);
-        webview.getSettings().setBuiltInZoomControls(true);
-        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webview.setInitialScale(50);
-
-        setTitle("Bülent Ecevit Üniversitesi Ana Sayfa");
+        webview.loadUrl("https://photosynth.net/preview/embed/283b433e-508f-41bd-b385-1a011d358a6c?delayload=false&autoplay=true&fromsite=true");
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ana_sayfa__web_page, menu);
-
+        getMenuInflater().inflate(R.menu.menu_beu_3dpage, menu);
         return true;
     }
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Toast.makeText(this, "Ana Sayfa yükleniyor...", Toast.LENGTH_LONG);
+
         // Checks the orientation of the screen for landscape and portrait and set portrait mode always
         if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
--- app/src/main/java/com/bilgiislem/sems/beunapp/Beu3D/Menu5_Fragment_Beu3D.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/Beu3D/Menu5_Fragment_Beu3D.java
@@ -30,19 +30,4 @@ public class Menu5_Fragment_Beu3D extends Fragment {
         return view;
     }
 
-
-    /*
-    View rootview;
-    private String Url = "https://photosynth.net/preview/embed/283b433e-508f-41bd-b385-1a011d358a6c?delayload=false&autoplay=true&fromsite=true";
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootview = inflater.inflate(R.layout.menu5_layout, container, false);
-        WebView webView = (WebView) rootview.findViewById(R.id.beu_3d_page);
-        webView.loadUrl(Url);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.setInitialScale(50);
-        return rootview;
-    }*/
 }
