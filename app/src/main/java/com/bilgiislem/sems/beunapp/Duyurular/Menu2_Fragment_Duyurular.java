1ca4dc712cc681a78796c1ccfcffc41590747783
semsturgut <2470417>
2015-04-27 02:45:47 +0300

    Duyurular %70 tamam düzeltilmelerin ardından diger bloklara gecilecek..

--- app/src/main/java/com/bilgiislem/sems/beunapp/Duyurular/Menu2_Fragment_Duyurular.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/Duyurular/Menu2_Fragment_Duyurular.java
@@ -1,15 +1,18 @@
 package com.bilgiislem.sems.beunapp.Duyurular;
 
+
 import android.app.Fragment;
 import android.app.ProgressDialog;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.bilgiislem.sems.beunapp.R;
 
@@ -18,14 +21,12 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 public class Menu2_Fragment_Duyurular extends Fragment {
     View rootview;
     Document doc;
-    WebView webView;
     String url_duyurular = "http://w3.beun.edu.tr/";
-    ProgressDialog mProgressDialog;
     String data = "";
     Element div;
 
@@ -33,61 +34,33 @@ public class Menu2_Fragment_Duyurular extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.menu2_layout_duyurular, container, false);
+        final WebView webView = (WebView) rootview.findViewById(R.id.duyurular_page);
+        webView.getSettings().setJavaScriptEnabled(true);
+        ThreadparsingUrl.start();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadData(data, "text/html", null);
+            }
+        });
 
-        new ListRefresher().execute();
-        webView.loadData(data, "text/html", null);
+        webView.loadUrl(url_duyurular);
+
         return rootview;
     }
 
-    private void LoadData(){
-        WebView webView = (WebView) rootview.findViewById(R.id.duyurular_page);
-        webView.getSettings().setJavaScriptEnabled(true);
-        try {
-            doc = Jsoup.connect(url_duyurular).get();
-            Element div = doc.select("#yazilar").get(0);
-            data += div;
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    protected Dialog onCreateDialog(int id) {
-        if(id == LOADING_DIALOG){
-            ProgressDialog loadingDialog = new ProgressDialog(this);
-            loadingDialog.setMessage("Loading records ...");
-            loadingDialog.setIndeterminate(true);
-            loadingDialog.setCancelable(true);
-            return loadingDialog;
-        }
-        return super.onCreateDialog(id);
-    }
-    private class ListRefresher extends AsyncTask {
+        Thread ThreadparsingUrl = new Thread() {
+            public void run() {
+                try {
+                    doc = Jsoup.connect(url_duyurular).get();
+                    div = doc.select("#yazilar").get(1);
+                    data += div;
+                    Log.i("Words", data);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
 
-        /**
-         * This is executed in the UI thread. The only place where we can show the dialog.
-         */
-        @Override
-        protected void onPreExecute() {
-            showDialog(LOADING_DIALOG);
-        }
-
-        /**
-         * This is executed in the background thread.
-         */
-        @Override
-        protected Void doInBackground(Uri... params) {
-            LoadData();
-            return null;
-        }
-
-        /**
-         * This is executed in the UI thread. The only place where we can show the dialog.
-         */
-        @Override
-        protected void onPostExecute(Void unused) {
-            dismissDialog(LOADING_DIALOG);
-        }
-    }
-
-}
 }
