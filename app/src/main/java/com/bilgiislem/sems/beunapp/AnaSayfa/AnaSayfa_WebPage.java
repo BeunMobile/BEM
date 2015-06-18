07dfd2627453588f081e27c083ae7fdcd3a041b6
semsturgut <mtdetroit@gmail.com>
2015-06-19 00:33:09 +0300

    Adding JSON in duyurular.

--- app/src/main/java/com/bilgiislem/sems/beunapp/AnaSayfa/AnaSayfa_WebPage.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/AnaSayfa/AnaSayfa_WebPage.java
@@ -2,129 +2,63 @@ package com.bilgiislem.sems.beunapp.AnaSayfa;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+
 import android.os.Bundle;
-import android.util.Log;
+
+import android.view.Window;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.bilgiislem.sems.beunapp.R;
 
+
 public class AnaSayfa_WebPage extends Activity {
 
-    private WebView webView;
+    WebView mWeb;
+    ProgressDialog mProgress;
 
+    @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ana_sayfa__web_page);
+        // no need to use title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // set webview as main content only
+        mWeb = new WebView(this);
+        setContentView(mWeb);
+        // set Javascript
+        WebSettings settings = mWeb.getSettings();
+        settings.setJavaScriptEnabled(true);
+        mWeb.setInitialScale(100);
+        settings.setBuiltInZoomControls(true);
 
-        webView = (WebView) findViewById(R.id.ana_sayfa_wp);
+        // the init state of progress dialog
+        mProgress = ProgressDialog.show(this, this.getString(R.string.loading), this.getString(R.string.waitfor));
 
-        startWebView("http://w3.beun.edu.tr/");
-
-    }
-
-    private void startWebView(String url) {
-        webView.setWebViewClient(new WebViewClient() {
-            ProgressDialog progressDialog;
-
+        // add a WebViewClient for WebView, which actually handles loading data from web
+        mWeb.setWebViewClient(new WebViewClient() {
+            // load url
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
 
-            public void onLoadResource(WebView view, String url) {
-                if (progressDialog == null) {
-                    progressDialog = new ProgressDialog(AnaSayfa_WebPage.this);
-                    progressDialog.setMessage("Yükleniyor...");
-                    progressDialog.show();
-                }
-            }
-
+            // when finish loading page
             public void onPageFinished(WebView view, String url) {
-                try {
-                    if (progressDialog.isShowing()) {
-                        progressDialog.dismiss();
-                        progressDialog = null;
-                    }
-                } catch (Exception exception) {
-                    exception.printStackTrace();
+                if (mProgress.isShowing()) {
+                    mProgress.dismiss();
                 }
             }
-
         });
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        webView.setScrollbarFadingEnabled(false);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.loadUrl(url);
-
+        // set url for webview to load
+        mWeb.loadUrl("http://w3.beun.edu.tr");
     }
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
+        if (mWeb.canGoBack())
+            mWeb.goBack();
+        else
             super.onBackPressed();
-        }
     }
-
 }
-
-
-/*
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebView;
-import android.widget.Toast;
-
-import com.bilgiislem.sems.beunapp.R;
-
-
-public class AnaSayfa_WebPage extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        final String AnaUrl="http://w3.beun.edu.tr/";
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ana_sayfa__web_page);
-        WebView webview = (WebView) findViewById(R.id.ana_sayfa_wp);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.canGoBack();
-        webview.canGoForward();
-        webview.loadUrl(AnaUrl);
-        webview.getSettings().setBuiltInZoomControls(true);
-        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webview.setInitialScale(50);
-
-        setTitle("Bülent Ecevit Üniversitesi Ana Sayfa");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-// Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ana_sayfa__web_page, menu);
-
-        return true;
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        Toast.makeText(this, "Ana Sayfa yükleniyor...", Toast.LENGTH_LONG);
-        // Checks the orientation of the screen for landscape and portrait and set portrait mode always
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        }
-    }
-}*/
--- app/src/main/java/com/bilgiislem/sems/beunapp/Duyurular/Menu2_Fragment_Duyurular.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/Duyurular/Menu2_Fragment_Duyurular.java
@@ -1,7 +1,10 @@
 package com.bilgiislem.sems.beunapp.Duyurular;
 
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,114 +12,154 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bilgiislem.sems.beunapp.NavigationAndMain.MainActivity;
 import com.bilgiislem.sems.beunapp.R;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.w3c.dom.Text;
 
-public class Menu2_Fragment_Duyurular extends Fragment {
-    Document doc;
-    WebView webView;
-    String url_duyurular = "http://w3.beun.edu.tr/";
-    ProgressBar progressBar_duyurular;
-    private Bundle webViewBundle;
-    String data = "";
-    Element div;
+public class Menu2_Fragment_Duyurular extends ListFragment {
+
+    private ProgressDialog pDialog;
+
+    // URL to get contacts JSON
+    private static String url = "http://w3.beun.edu.tr/mobil-duyurular/";
+
+    // JSON Node names
+    private static final String TAG_S1 = "s1";
+    private static final String TAG_BASLIK = "baslik";
+    private static final String TAG_ADRES = "adres";
+
+
+    JSONArray contacts = null;
+
+
+    ArrayList<HashMap<String, String>> contactList;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.menu2_layout_duyurular, container, false);
 
-        progressBar_duyurular = (ProgressBar) view.findViewById(R.id.duyurular_progress);
-        webView = (WebView) view.findViewById(R.id.duyurular_page);
-        webView.getSettings().setJavaScriptEnabled(true);
-        ThreadparsingUrl.start();
-        webView.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                webView.loadUrl(url_duyurular);
 
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                // TODO Auto-generated method stub
-                super.onPageStarted(view, url, favicon);
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                // TODO Auto-generated method stub
-
-                webView.loadUrl(url);
-                return true;
-
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // TODO Auto-generated method stub
-                super.onPageFinished(view, url);
-
-                progressBar_duyurular.setVisibility(View.GONE);
-                webView.loadData(data, "text/html; charset=utf-8", "utf-8");
-            }
-        });
-        webView.loadUrl(url_duyurular);
-
-        // Just load whatever URL this fragment is
-        // created with.
         return view;
     }
 
-    // This is the method the pager adapter will use
-    // to create a new fragment
-    public static Fragment newInstance(String url) {
-        Menu2_Fragment_Duyurular f = new Menu2_Fragment_Duyurular();
-        f.url_duyurular = url;
-        return f;
-    }
-
-    /**
-     * Sla webview op
-     */
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        webViewBundle = new Bundle();
-        webView.saveState(webViewBundle);
-    }
-
-    /**
-     * Herstel staat van webview
-     */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
 
-        if (webViewBundle != null) {
-            webView.restoreState(webViewBundle);
-        }
+
+        contactList = new ArrayList<HashMap<String, String>>();
+
+        ListView listView = getListView();
+
+        // Calling async task to get json
+        new GetContacts().execute();
+
+
+        super.onActivityCreated(savedInstanceState);
     }
 
-    Thread ThreadparsingUrl = new Thread() {
-        public void run() {
-            try {
-                doc = Jsoup.connect(url_duyurular).get();
-                div = doc.select("#yazilar").get(0);
-                data += div;
-                Log.i("Words", data);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(getActivity());
+            pDialog.setMessage(getActivity().getString(R.string.waitfor));
+            pDialog.setCancelable(false);
+            pDialog.show();
+
         }
-    };
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+
+            Log.d("Response: ", "> " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    contacts = jsonObj.getJSONArray(TAG_S1);
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String baslik = c.getString(TAG_BASLIK);
+                        String adres = c.getString(TAG_ADRES);
+
+
+                        // tmp hashmap for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put(TAG_BASLIK, baslik);
+                        contact.put(TAG_ADRES, adres);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+
+            ListAdapter adapter = new SimpleAdapter(
+                    getActivity(), contactList,
+                    R.layout.json_items, new String[]{TAG_BASLIK, TAG_ADRES}, new int[]{R.id.name, R.id.adres});
+
+            setListAdapter(adapter);
+        }
+
+
+    }
+
 
 }
