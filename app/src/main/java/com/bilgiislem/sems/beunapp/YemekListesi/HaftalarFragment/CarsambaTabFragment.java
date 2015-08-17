3d9bf7891fb0e7106832179c8a2c1dd69e4eaf33
semsturgut <mtdetroit@gmail.com>
2015-08-17 10:35:04 +0300

    1-Yemek Listesi Kodları %80 bitti tarih ayarlamaları yapılıyor. 2-Hafta içi fotoğraflar çekilecek. 3-Küçük tasarım öğeleri değiştirilecek butonlar toast mesajlar pop up mesajlar vsvs.(Süresi Bilinmiyor)

--- app/src/main/java/com/bilgiislem/sems/beunapp/YemekListesi/HaftalarFragment/CarsambaTabFragment.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/YemekListesi/HaftalarFragment/CarsambaTabFragment.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +20,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -46,10 +51,10 @@ public class CarsambaTabFragment extends Fragment {
 
     Calendar localCalendar = Calendar.getInstance(TimeZone.getDefault());
     final int currentDay = localCalendar.get(Calendar.DATE);
-    final int currentDayOfWeek = localCalendar.get(Calendar.DAY_OF_WEEK);
+    final int dayOfWeek = localCalendar.get(Calendar.DAY_OF_WEEK);
+    final int weekOfMonth = localCalendar.get(Calendar.WEEK_OF_MONTH);
     final int currentMonth = localCalendar.get(Calendar.MONTH) + 1;
     final int currentYear = localCalendar.get(Calendar.YEAR);
-    final int dayOfWeek = localCalendar.get(Calendar.DAY_OF_WEEK);
 
 
     String url = "http://w3.beun.edu.tr/yemek_listesi/veri/?ay=" + currentMonth + "&yil=" + currentYear;
@@ -76,26 +81,48 @@ public class CarsambaTabFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.carsamba_tab, container, false);
 
-       /* corbaCalText = (TextView) view.findViewById(R.id.corba_cal);
+        corbaText = (TextView) view.findViewById(R.id.corba_text);
+        corbaCalText = (TextView) view.findViewById(R.id.corba_cal);
         yemek1Text = (TextView) view.findViewById(R.id.yemek1_text);
         yemek1CalText = (TextView) view.findViewById(R.id.yemek1_cal);
         yemek2Text = (TextView) view.findViewById(R.id.yemek2_text);
         yemek2CalText = (TextView) view.findViewById(R.id.yemek2_cal);
         digerText = (TextView) view.findViewById(R.id.diger_text);
-        digerCalText = (TextView) view.findViewById(R.id.diger_cal);*/
+        digerCalText = (TextView) view.findViewById(R.id.diger_cal);
 
-        corbaText = (TextView) view.findViewById(R.id.corba_text);
+        corbaText.setVisibility(View.GONE);
+        corbaCalText.setVisibility(View.GONE);
+        yemek1Text.setVisibility(View.GONE);
+        yemek1CalText.setVisibility(View.GONE);
+        yemek2Text.setVisibility(View.GONE);
+        yemek2CalText.setVisibility(View.GONE);
+        digerText.setVisibility(View.GONE);
+        digerCalText.setVisibility(View.GONE);
 
+/*
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, currentYear);
+        cal.set(Calendar.MONTH, currentMonth+1);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
+        int start = cal.get(Calendar.WEEK_OF_YEAR);
+        cal.set(Calendar.YEAR, currentYear);
+        cal.set(Calendar.MONTH, currentMonth+1);
+        cal.set(Calendar.DAY_OF_MONTH, 27);
+        int end = cal.get(Calendar.WEEK_OF_YEAR);
+        int weekly = end - start + 1;
+        Log.d("weekly", "" + weekly);
+        Log.d("DOW", "" + dayOfWeek + "/" + currentDay);*/
 
-        new JSONParse().execute();
+        if (dayOfWeek != 0 && dayOfWeek != 7) {
 
+            new JSONParse().execute();
+
+        }
         return view;
     }
 
 
     private class JSONParse extends AsyncTask<Void, Void, Void> {
-        private ProgressDialog pDialog;
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -148,7 +175,76 @@ public class CarsambaTabFragment extends Fragment {
         @Override
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
-            corbaText.setText(corbaList.get(2));
+            //Log.d("WOM", "" + weekOfMonth);
+
+            try {
+                Log.d("GNOFS", " " + getNumberofSundays(currentYear + "-" + currentMonth + "-" + 1, currentYear + "-" + currentMonth + "-" + currentDay));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            corbaText.setText(corbaList.get(12));
+            corbaCalText.setText(corbaCalList.get(12));
+            yemek1Text.setText(yemek1List.get(12));
+            yemek1CalText.setText(yemek1CalList.get(12));
+            yemek2Text.setText(yemek2List.get(12));
+            yemek2CalText.setText(yemek2CalList.get(12));
+            digerText.setText(digerList.get(12));
+            digerCalText.setText(digerCalList.get(12));
+
+
+            corbaText.setVisibility(View.VISIBLE);
+            corbaCalText.setVisibility(View.VISIBLE);
+            yemek1Text.setVisibility(View.VISIBLE);
+            yemek1CalText.setVisibility(View.VISIBLE);
+            yemek2Text.setVisibility(View.VISIBLE);
+            yemek2CalText.setVisibility(View.VISIBLE);
+            digerText.setVisibility(View.VISIBLE);
+            digerCalText.setVisibility(View.VISIBLE);
         }
+    }
+
+
+    public int getNumberofSundays(String d1, String d2) throws Exception { // object
+        // in
+        // Date
+        // form
+
+        Date date1 = getDate(d1);
+        Date date2 = getDate(d2);
+
+        Calendar c1 = Calendar.getInstance();
+        c1.setTime(date1);
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(date2);
+        int mondays = 0;
+        while (c2.after(c1)) {
+            // System.out.println(" came here ");
+            //checks to see if the day1 ....so on next days are sundays if sunday goes inside to increment the counter
+            if (c1.get(Calendar.DAY_OF_WEEK) == Calendar.MONDAY) {
+                System.out.println(c1.getTime().toString() + " is a monday ");
+                mondays++;
+
+            }
+            c1.add(Calendar.DATE, 1);
+        }
+
+        Log.d("GNOFB2S", "number of sundays between 2 dates is " + mondays);
+
+        return mondays;
+    }
+
+    // converts string to date
+    public Date getDate(String s) {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try {
+            date = format.parse(s);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return date;
     }
 }
