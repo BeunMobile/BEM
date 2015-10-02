4d05de88ac8d0d8a716a87b1d8dae94f8a07d7e2
semsturgut <mtdetroit@gmail.com>
2015-10-02 10:33:26 +0300

    Marshmallow güncellemeleri yapıldı.

--- app/src/main/java/com/bilgiislem/sems/beunapp/DHE_Sources/ServiceHandler.java
+++ app/src/main/java/com/bilgiislem/sems/beunapp/DHE_Sources/ServiceHandler.java
@@ -4,10 +4,6 @@ package com.bilgiislem.sems.beunapp.DHE_Sources;
  * Created by sems on 18.06.2015.
  */
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -18,6 +14,11 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 
 public class ServiceHandler {
 
